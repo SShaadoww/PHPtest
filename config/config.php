fe2b68e2342cf8d1830d2446a613d266d3d9f478
SShaadoww <128670051+SShaadoww@users.noreply.github.com>
2025-09-04 23:04:24 +0500

    Delete config/config.php

--- config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'db' => [
-        'dsn'  => 'mysql:host=localhost;dbname=testlibrary;charset=utf8mb4',
-        'user' => 'root',
-        'pass' => 'Evropa20',
-    ],
-    'jwt_secret' => 'my_super_secret_key'
-];
